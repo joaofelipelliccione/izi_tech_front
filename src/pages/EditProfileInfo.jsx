5f561893a73113434ee8070eafc756bc4742d365
João Felipe P <jf.pelliccione@gmail.com>
2022-03-17 20:45:10 -0300

    Consertando problema com URL de Imagem.

--- src/pages/EditProfileInfo.jsx
+++ src/pages/EditProfileInfo.jsx
@@ -150,8 +150,8 @@ function EditProfileInfo() {
       navigate('/');
       swal('Sessão expirada :(', 'Por favor, realize um novo login.', 'info');
     } else {
-      infoToUpdate.userPicture = `https://storage.googleapis.com/gcsb-izi-tech-profile-pictures/${loginInfo.userId}.jpeg`;
-      loginInfoFromLS.userPicture = `https://storage.googleapis.com/gcsb-izi-tech-profile-pictures/${loginInfo.userId}.jpeg`;
+      infoToUpdate.userPicture = cleanData.imgSrc;
+      loginInfoFromLS.userPicture = cleanData.imgSrc;
       localStorage.setItem('loginInfo', JSON.stringify(loginInfoFromLS));
       dispatch(setLoginInfoAC(loginInfoFromLS));
     }
